60b3bf191e5d747e5eb83c452e9eae4925705e02
Ayush Sanyal <a.sanyal902@gmail.com>
2013-04-20 21:55:33 +0530

    Second commit

---
+++ SpaceInvaders/src/org/newdawn/spaceinvaders/Menu.java
@@ -0,0 +1,14 @@
+package org.newdawn.spaceinvaders;
+
+import java.applet.Applet;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Menu{
+
+this.add(onePlayer);
+}
