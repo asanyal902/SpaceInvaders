e37e18948f400ca23f45decdb5f48820e7cfcb96
Ayush Sanyal <a.sanyal902@gmail.com>
2013-04-22 18:21:40 +0530

    Trying out adding intial menu to game

--- SpaceInvaders/src/org/newdawn/spaceinvaders/Game.java
+++ SpaceInvaders/src/org/newdawn/spaceinvaders/Game.java
@@ -74,7 +74,7 @@ public class Game extends Canvas {
         /*Time between boss alien firing shots*/
     private long fireInterval = 1000;
     /* level of game to play*/
-    private int level = 4;
+    private int level = 0;
     /*label for player1 name*/
     private JLabel player1 = new JLabel("Chirayu");
     /*label for player2 name*/
@@ -562,9 +562,19 @@ public class Game extends Canvas {
 	 * @param argv The arguments that are passed into our game
 	 */
 	public static void main(String argv[]) {
+		Menu.playInit();
+	    Menu m = new Menu();
+	 JFrame f = new JFrame();
+	//f.setLayout(new BorderLayout());
+	 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+	f.setSize(800,800);
+	f.setVisible(true);
+	f.setContentPane(m);
 		Game g =new Game();
 
-		// Start the main game loop, note: this method will not
+		 //Start the main game loop, note: this method will not
 		// return until the game has finished running. Hence we are
 		// using the actual main thread to run the game.
 		g.gameLoop();
